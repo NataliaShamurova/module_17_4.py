6b68d622ae96a17be5395d2967e9a2604d310a11
NataliaShamurova <shamurovann@yandex.ru>
2024-11-01 21:40:23 +0300

    dz17_1

---
+++ app/main.py
@@ -0,0 +1,16 @@
+#uvicorn main:app --reload
+from fastapi import FastAPI
+
+from routers import task
+from routers import user
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(task.router)
+app.include_router(user.router)
