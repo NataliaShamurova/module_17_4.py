ea9f7ef7544cf4bb2a2719b34819ad0836cb506e
NataliaShamurova <shamurovann@yandex.ru>
2024-11-02 12:48:19 +0300

    dz17_4

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine('sqlite:///taskmanager.db')
 
-Session_local = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 
 class Base(DeclarativeBase):
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-#uvicorn main:app --reload
+#uvicorn app.main:app --reload
 from fastapi import FastAPI
 
-from routers import task
-from routers import user
+from app.routers import task
+from app.routers import user
 
 app = FastAPI()
 
